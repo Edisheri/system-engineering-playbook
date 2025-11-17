ae83f995d421850c7e9523aa097448acd0d52afc
Edisheri <kipiani182@gmail.com>
2025-11-17 14:51:45 +0400

    Add inline styles to Draw.io iframe for larger size (800px) and remove CSS dependency

--- _tools/mdbook-drawio/src/main.rs
+++ _tools/mdbook-drawio/src/main.rs
@@ -20,7 +20,7 @@ impl DrawioPreprocessor {
     fn create_iframe(&self, url: &str) -> String {
         let raw_url = self.convert_github_to_raw(url);
         format!(
-            r#"<iframe class="drawio-viewer" src="https://viewer.diagrams.net/?highlight=0000ff&edit=_blank&layers=1&nav=1&title=diagram&url={}"></iframe>"#,
+            r#"<iframe class="drawio-viewer" style="width: 100%; height: 800px; min-height: 600px; border: 1px solid #ddd; border-radius: 4px; margin: 20px 0;" src="https://viewer.diagrams.net/?highlight=0000ff&edit=_blank&layers=1&nav=1&title=diagram&url={}"></iframe>"#,
             raw_url
         )
     }
