f15a0fa6521669304a94b27fc05fdd995b9a07e8
Edisheri <Edisheri@users.noreply.github.com>
2025-11-24 18:40:16 +0000

    deploy: e2bd40164aca891e0b5bdac326575353de5e1758

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c1c4d42c.js',
+                path_to_root + 'searchindex-4a1ca1b4.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
