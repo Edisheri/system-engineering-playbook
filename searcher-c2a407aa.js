b7fbd125046cbf61baa8b19d92cf2ded6f816001
yamakassi <yamakassi@users.noreply.github.com>
2025-12-25 08:30:46 +0000

    deploy: 4e2f0cc3489ed84e256191191080d94305ee16d0

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2f12467f.js',
+                path_to_root + 'searchindex-6b78b7d6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
